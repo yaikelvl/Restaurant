036b134e0286d58051f7b411d94ffa81825fafe6
Yaikel <yaikelvl@gmail.com>
2024-10-31 17:43:10 -0400

    first comit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ClientModule } from './client/client.module';
+import { RestaurantModule } from './restaurant/restaurant.module';
+import { OrderModule } from './order/order.module';
+
+
+@Module({
+  imports: [ClientModule, RestaurantModule, OrderModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
